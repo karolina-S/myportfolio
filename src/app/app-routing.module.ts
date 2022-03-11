08ae5c364007c44ac23f921d29f59d1e13a0c284
Karolina Stefańczyk <karolina.m.ste@gmail.com>
2022-03-11 17:20:36 +0100

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutmeComponent } from './aboutme/aboutme.component';
+import { ContactComponent } from './contact/contact.component';
+import { MainpageComponent } from './mainpage/mainpage.component';
+import { MyskillsComponent } from './myskills/myskills.component';
+import { MyportfolioComponent } from './myportfolio/myportfolio.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: MainpageComponent },
+  { path: 'home', component: MainpageComponent },
+  { path: 'aboutme', component: AboutmeComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'myskills', component: MyskillsComponent },
+  { path: 'myportfolio', component: MyportfolioComponent },
+  { path: 'navbar', component: NavbarComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { AboutmeComponent } from './aboutme/aboutme.component';
+import { ContactComponent } from './contact/contact.component';
+import { MainpageComponent } from './mainpage/mainpage.component';
+import { MyskillsComponent } from './myskills/myskills.component';
+import { MyportfolioComponent } from './myportfolio/myportfolio.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    AboutmeComponent,
+    ContactComponent,
+    MainpageComponent,
+    MyskillsComponent,
+    MyportfolioComponent
   ],
   imports: [
     BrowserModule,
